d6be78af792029634e4467b03c18540f40819e24
Memo Chou <memochou1993@gmail.com>
2022-06-14 16:22:19 +0800

    Add lint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: 'airbnb',
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: '2020',
+  },
+  rules: {
+    'no-undef': 'off',
+  },
+};
